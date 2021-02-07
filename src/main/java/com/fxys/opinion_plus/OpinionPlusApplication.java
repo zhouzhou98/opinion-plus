07af009b53fc0ab0c0094f8dc7e7f0f307f355ce
1404031711@qq.com <1404031711@qq.com>
2021-02-07 08:14:02 +0800

    '项目初始化'

---
+++ src/main/java/com/fxys/opinion_plus/OpinionPlusApplication.java
@@ -0,0 +1,13 @@
+package com.fxys.opinion_plus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpinionPlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpinionPlusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fxys/opinion_plus/OpinionPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fxys.opinion_plus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpinionPlusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
